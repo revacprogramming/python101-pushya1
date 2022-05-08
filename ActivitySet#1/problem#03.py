e91f1f05a6b34a8c9a402d981408289034bd12f7
pushya1 <ss123maila@gmail.com>
2022-05-08 12:14:53 +0530

    finding gross pay

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,10 @@
 # Variables, Expressions & Statements
+# This first line is provided for you
 
-hrs = float(input("Enter hours? "))
+hrs = float(input("Enter Hours:"))
+
+rate = float(input("Enter rate:"))
+
+pay = hrs*rate
+
+print(f'Pay: {pay}')
