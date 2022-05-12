2ff746a6f223f652296d2f62a990ec1e15f09937
pushya1 <ss123maila@gmail.com>
2022-05-13 00:11:38 +0530

    #lists

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,24 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+# filename = "dataset/romeo.txt"
+
+fname = input("Enter file name: ")
+
+try:
+    fhand = open(fname)
+except:
+    print("the file not exist")
+    quit()
+fh = fhand.read()
+words_sp=fh.split()
+
+lst = []
+
+for word in words_sp:
+    if word not in lst:
+        lst.append(word)
+    else:
+        continue
+
+lst.sort()
+print(lst)
