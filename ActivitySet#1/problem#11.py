d22f02318ce042bec3e3571dbee8739e8b6a9c26
pushya1 <ss123maila@gmail.com>
2022-05-15 10:55:18 +0530

    #tuples

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,26 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+hours = list()
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From '):
+        word = line.split(":")
+        hours.append(word[0][-2:])
+hours.sort()
+
+di = dict()
+
+for hour in hours:
+    di[hour] = di.get(hour,0)+1
+    
+for k,v in di.items():
+    print(k,v)
+
+        
