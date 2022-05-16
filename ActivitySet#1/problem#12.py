e1a7dc1cc2dc7d211c5ec9c6db748265d5647e02
pushya1 <ss123maila@gmail.com>
2022-05-16 18:14:17 +0530

    #Regular Expressions

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,18 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+
+fhand = open("actual-file")
+import re
+nums_list = list()
+for line in fhand:
+    line = line.rstrip()
+    num_list=re.findall('[0-9]+',line)
+    if len(num_list)==0:
+        continue
+    nums_list+=num_list
+total = 0
+for num in nums_list:
+    total+=int(num)
+print("sum of the numbers in sample text is ",total)
+    
+    
