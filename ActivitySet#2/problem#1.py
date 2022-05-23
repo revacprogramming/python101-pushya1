996f8508af1cd047f08a5b266352beca489401e1
pushya1 <ss123maila@gmail.com>
2022-05-23 16:05:18 +0530

    #adding numbers

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,22 @@
 
 
-def add(a, b):
-    return  # ...
+# def add(a, b):
+#     return  # ...
 
 
-def main():
-    a = input  # ...
-    b = input  # ...
+# def main():
+#     a = input  # ...
+#     b = input  # ...
 
-    c = add(a, b)
-    print  # ...
+#     c = add(a, b)
+#     print  # ...
+
+a = int(input("Enter  1st number:"))
+b = int(input("Enter 2nd  number:"))
+
+def add(a,b):
+    c = a+b
+    print('sum of',a,'and',b,'is',c)
+
+
+add(a,b)
