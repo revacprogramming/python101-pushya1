bbcdd2c15c1f0a2e231e3c4287dc35b07efaf589
pushya1 <ss123maila@gmail.com>
2022-07-06 20:32:30 +0530

    split string

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -2,15 +2,18 @@
 
 def get_cs():
     """get string input"""
+    cs=input("Enter a string:")
+    return cs
 
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
+    ls=cs.split()
+    return ls
 
 
 def main():
     cs = get_cs()
-
     lot = cs_to_lot(cs)
     print(lot)
 
