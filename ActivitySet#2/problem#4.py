5debf709286fd660c409a76c7992530a8e1f520c
pushya1 <ss123maila@gmail.com>
2022-07-06 20:43:45 +0530

    #list to sting

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -2,14 +2,21 @@
 
 def get_cs():
     """get string input"""
-
+    cs=input("Enter a string:")
+    return cs
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-
+    lot=cs.split()
+    return lot
 
 def lot_to_cs(lot):
     """convert list of strings to connected string"""
+    string=""
+    for word in lot:
+        string=string+word+" "
+    return string
+
 
 
 def main():
