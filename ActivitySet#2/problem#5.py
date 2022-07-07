780b0e700a0b94c9c04bbdde4bf397c25e48e5f1
pushya1 <ss123maila@gmail.com>
2022-07-07 21:49:24 +0530

    #dictionarys

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,16 +1,28 @@
 
-
 def get_cs():
     """get string input"""
+    cs = input("Enter a string")
+    return cs
 
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
-
-
+    ls = cs.split()
+    d=dict()
+    i=0
+    for word in ls:
+        d[i]=word
+        i+=1
+    return d
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
-
+    i=0
+    cs=""
+    for i in d:
+        if i<len(d):
+            cs+=d[i]+" "
+            i=i+1
+    return cs
 
 def main():
     cs = get_cs()
