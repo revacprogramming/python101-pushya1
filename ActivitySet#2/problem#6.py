edcf3d3cf6c46fee59b735afbf7dc9bc7e972cc7
pushya1 <ss123maila@gmail.com>
2022-07-11 18:47:33 +0530

    #oop

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -2,10 +2,18 @@
 
 class Menu:
     """fill in class definition here"""
+    """fill in class definition here"""
+    di = dict()
+    def add(self,name,cost):
+        self.di[name]=cost
+    def show(self):
+        for every in self.di:
+            print(every,',',self.di[every])
+
 
 
 m = Menu()  # Menu is a class
 m.add("idly", 10)
-m.add("vada". 20)
+m.add("vada", 20)
 
 m.show()
