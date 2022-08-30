885e57930d54364f6ba732316176f834d28c4e78
pushya1 <ss123maila@gmail.com>
2022-08-30 16:56:40 +0530

    #8

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -17,20 +17,31 @@
 # m.add("vada", 20)
 
 # m.show()
+#===========================================================================================================================================
+
+# class Menu:
+#     """fill in class definition here"""
+
+
+
+# m = Menu()
+# m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
+
+# print(m)  # should print the menu properly
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 class Menu:
-    """fill in class definition here"""
-
-
-
-m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
-
-
-
-
+    
+    def __init__(self):
+        di={}
+    def __add__(self,key,value):
+        self.di[key]=value
+    def __str__(self):
+        string = ""
+        for k,v in self.di.items():
+            string+=f'{k} {v} \n'
+        return string
 
 
 
